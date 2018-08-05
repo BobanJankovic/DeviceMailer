45679e4aadcb08564023a77658dd17410bffb384
bobi <boban0092@gmail.com>
2018-08-05 22:38:50 +0200

    Picture uploading

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_02_201521) do
+ActiveRecord::Schema.define(version: 2018_08_05_202440) do
+
+  create_table "images", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "picture"
+    t.index ["user_id"], name: "index_images_on_user_id"
+  end
 
   create_table "payments", force: :cascade do |t|
     t.string "email"
